28c7c389402bba13e6228d59f7d9cc50732f994b
Peter Mullins <jc310xc@gmail.com>
2022-01-10 03:02:53 -0500

    complete happy-path for 1 hand of poker

--- Poker.py
+++ Poker.py
@@ -1,6 +1,7 @@
 class Player:
     sittingOut = False
     hasFolded = False
+    isSelf = False
     turnHistory = []
 
     def __init__(self, name):
@@ -10,5 +11,28 @@ class Hand:
     cards = []
 
     def __init__(self, card1, card2):
-        cards.append(card1)
-        cards.append(card2)
+        self.cards.append(card1)
+        self.cards.append(card2)
+
+    def toString(self):
+        return f"{self.cards[0]} {self.cards[1]}"
+
+class Action:
+    def __init__(self, playerName, action):
+        self.playerName = playerName
+        self.action = action
+
+    def getReadableAction(self, action):
+        if(action == "f"):
+            return "folded"
+        if(action == "ch"):
+            return "check"
+        if(action == "c"):
+            return "call"
+        if(action[0] == "b"):
+            return f"bet {action[1:]}"
+        if(action[0] == "r"):
+            return f"raise {action[1:]}"
+
+    def toString(self):
+        return f"{self.playerName}: {self.getReadableAction(self.action)}"
--- hand-tracker.py
+++ hand-tracker.py
@@ -1,5 +1,5 @@
 import os
-from Poker import Player
+from Poker import Player, Hand, Action
 
 maxPlayers = 6
 numStreets = 4
@@ -7,8 +7,7 @@ smallBlind = 0
 bigBlind = 0
 numCommands = 4
 players = [None] * maxPlayers
-playerHistories = []
-buttonPlayerIdx = 0
+handHistories = []
 
 def initPlayers():
     global players, buttonPlayerIdx
@@ -16,8 +15,11 @@ def initPlayers():
         playerName = input(f"Input name for player {i+1} or enter to skip: ")
         if(playerName):
             players[i] = Player(playerName)
-
-        buttonPlayerIdx = int(input("Which player is on the button?"))
+            if(i == 0):
+                players[i].isSelf = True
+        else:
+            players[i] = Player("")
+            players[i].sittingOut = True
 
 def printMenu():
     print("1. Start hand")
@@ -25,7 +27,7 @@ def printMenu():
     print("3. View Past Hands")
     print("4. Quit")
 
-def acquireNumericalInput(maxChoice):
+def acquireInput(maxChoice):
     validInput = False
     while(not validInput):
         try:
@@ -41,31 +43,92 @@ def acquireNumericalInput(maxChoice):
 
     return userInput
 
+def getStreetName(i):
+    if(i == 0):
+        return "Preflop"
+    elif(i == 1):
+        return "Flop"
+    elif(i == 2):
+        return "Turn"
+    elif(i == 3):
+        return "River"
+    else:
+        return "Invalid street"
+
+def getCommunityCards(street, communityCards):
+    if(street == 1):
+        communityCards += input("Input flop cards separated by a space: ").split()
+    if(street == 2):
+        communityCards.append(input("Input turn card: "))
+    if(street == 3):
+        communityCards.append(input("Input river card: "))
+
+def showActionsByStreet(streetActions):
+    count = 0
+    for street in streetActions:
+        print(f"{getStreetName(count)}")
+        for action in street:
+            if(action.action != "o"):
+                print(action.toString())
+        count = count + 1
+
 def trackHand():
     global buttonPlayerIdx, maxPlayers, players
     holeCard1 = input("Input first hole card: ")
     holeCard2 = input("Input second hole card: ")
+    startingPlayerIdx = int(input("Which seat is UTG? ")) - 1
 
     hand = Hand(holeCard1, holeCard2)
+    communityCards = []
+    actionsByStreet = []
+    playerHands = []
+    for player in players:
+        if(not player.sittingOut):
+            player.hasFolded = False
 
     for i in range(numStreets):
-        bettingClosed = False
-        playerIdx = (buttonPlayerIdx + 3) % maxPlayers
-        while(not bettingClosed):
+        cont = True
+        if(i != 0):
+            playerIdx = (startingPlayerIdx - 2) % maxPlayers
+        else:
+            playerIdx = startingPlayerIdx
+        playerActions = []
+
+        if(i > 0):
+            getCommunityCards(i, communityCards)
+
+        while(cont):
             currentPlayer = players[playerIdx]
             if(currentPlayer.hasFolded or currentPlayer.sittingOut):
-                action = "out"
+                action = "o"
             else:
-                action = input(f"Input player {playerIdx} ({players[playerIdx].name})'s action: ")
+                action = input(f"({getStreetName(i)}) Input player {playerIdx} ({players[playerIdx].name})'s action: ")
 
-
+            if(action.lower() == "end" or action.lower() == "e"):
+                cont = False
+            else:
+                if(action.lower() == "fold" or action.lower() == "f"):
+                    currentPlayer.hasFolded = True
+                if(action.lower() == "o"):
+                    currentPlayer.sittingOut = True
+                playerActions.append(Action(players[playerIdx].name, action))
 
             playerIdx = (playerIdx + 1) % maxPlayers
 
+        actionsByStreet.append(playerActions)
+
+    for player in players:
+        if(not (player.hasFolded or player.sittingOut or player.isSelf)):
+            playerHands.append(input(f"Input {player.name}'s cards, separated by spaces: ").split())
+
+    print("Your hand: " + hand.toString())
+    print(communityCards)
+    print(playerHands)
+    showActionsByStreet(actionsByStreet)
+
 def processInput(cmd):
-    if(cmd == 1):
+    if(cmd == "1"):
         trackHand()
-        printHandInfo()
 
 cont = True
 cmd = ""
@@ -75,7 +138,7 @@ initPlayers()
 while(cont):
     os.system('cls')
     printMenu()
-    cmd = acquireNumericalInput(numCommands)
+    cmd = acquireInput(numCommands)
     if(cmd == "q"):
         cont = False
     else:
